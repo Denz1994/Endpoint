09230290f26e81903d06ce87eece0ef924f2cca6
denz1994 <denz1994@gmail.com>
2024-12-08 10:36:57 -0500

    Lazy load todo list to test suspense fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css'
-import TodoList from './TodoList'
-import { Suspense } from 'react'
+import React, { Suspense } from 'react'
 
 function Loading() {
-  return <h2> ⏳ Loading...</h2>;
+  return <h2>⏳ Loading...</h2>;
 }
+
+// Simulates a delay in the component. Not the actual todo list api call, just the component's code loading.
+const LazyTodoList = React.lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import("./TodoList")), 1000);
+  });
+});
 function App() {
   return (
     <Suspense fallback={<Loading />}>
-    <TodoList/>
+    <LazyTodoList/>
     </Suspense>
   )
 }
