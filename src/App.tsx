bba716d01a5aa25f6531b4e079d48f49f28330ba
denz1994 <denz1994@gmail.com>
2024-12-08 10:20:07 -0500

    Wrap todo list in suspense for loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css'
 import TodoList from './TodoList'
+import { Suspense } from 'react'
 
+function Loading() {
+  return <h2> ⏳ Loading...</h2>;
+}
 function App() {
   return (
+    <Suspense fallback={<Loading />}>
     <TodoList/>
+    </Suspense>
   )
 }
 
