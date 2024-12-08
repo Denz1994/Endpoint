5afc698a78131479abf1236510999f8b404a5ca0
denz1994 <denz1994@gmail.com>
2024-12-08 09:28:12 -0500

    Add list of sample todos

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,7 +1,20 @@
+import {useState} from 'react';
+
+const testTodos = ['A', 'Some other task', 'New Item here'];
 function TodoList(){
-    return (<>
-    <p>hi</p>
-    </>)
+    // TODO: Separate into 3 lists: overdue, completed, not completed
+    const [todos, setTodos] = useState(testTodos);
+
+    return (
+    <div className="todo-list">
+        <h1 className ="list-header">Todos</h1>
+        <ul className ="list-items">
+        {todos.map((todo, index)=>{
+            return(<li key={index} className="list-item">{todo}</li>)
+            })
+        }
+        </ul>  
+    </div>)
 }
 
 export default TodoList;
